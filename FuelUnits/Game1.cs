c4ab349b3e32afb047aceb6c0e0152f979545a56
Tythor <tthorin@live.com>
2017-10-28 14:40:05 -0700

    Inital commit

---
+++ FuelUnits/Game1.cs
@@ -0,0 +1,98 @@
+﻿using System;
+
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace FuelUnits
+{
+	/// <summary>
+	/// This is the main type for your game.
+	/// </summary>
+	public class Game1 : Game
+	{
+		GraphicsDeviceManager graphics;
+		SpriteBatch spriteBatch;
+
+        Texture2D texture2D;
+
+		public Game1()
+		{
+			graphics = new GraphicsDeviceManager(this);
+			Content.RootDirectory = "Content";
+		}
+
+		/// <summary>
+		/// Allows the game to perform any initialization it needs to before starting to run.
+		/// This is where it can query for any required services and load any non-graphic
+		/// related content.  Calling base.Initialize will enumerate through any components
+		/// and initialize them as well.
+		/// </summary>
+		protected override void Initialize()
+		{
+			// TODO: Add your initialization logic here
+
+			base.Initialize();
+		}
+
+		/// <summary>
+		/// LoadContent will be called once per game and is the place to load
+		/// all of your content.
+		/// </summary>
+		protected override void LoadContent()
+		{
+			// Create a new SpriteBatch, which can be used to draw textures.
+			spriteBatch = new SpriteBatch(GraphicsDevice);
+
+			//TODO: use this.Content to load your game content here 
+            using (var stream = TitleContainer.OpenStream("Content/Tai_Lopez.jpg"))
+            {
+                texture2D = Texture2D.FromStream(this.GraphicsDevice, stream);
+
+            }
+        }
+
+		/// <summary>
+		/// Allows the game to run logic such as updating the world,
+		/// checking for collisions, gathering input, and playing audio.
+		/// </summary>
+		/// <param name="gameTime">Provides a snapshot of timing values.</param>
+		protected override void Update(GameTime gameTime)
+		{
+			// For Mobile devices, this logic will close the Game when the Back button is pressed
+			// Exit() is obsolete on iOS
+#if !__IOS__ && !__TVOS__
+			if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+				Exit();
+#endif
+			// TODO: Add your update logic here
+            Console.Write("Updating");
+
+			base.Update(gameTime);
+		}
+
+		/// <summary>
+		/// This is called when the game should draw itself.
+		/// </summary>
+		/// <param name="gameTime">Provides a snapshot of timing values.</param>
+		protected override void Draw(GameTime gameTime)
+		{
+			graphics.GraphicsDevice.Clear(Color.Black);
+
+			//TODO: Add your drawing code here
+            spriteBatch.Begin();
+
+            int screenCenterX = GraphicsDevice.Viewport.Bounds.Width / 2 - texture2D.Width / 2;
+            int screenCenterY = GraphicsDevice.Viewport.Bounds.Height / 2 - texture2D.Height / 2;
+
+            Vector2 position = new Vector2(screenCenterX, screenCenterY);
+            Color color = Color.White;
+
+            spriteBatch.Draw(texture2D, position, color);
+
+            spriteBatch.End();
+
+			base.Draw(gameTime);
+		}
+	}
+}
